ff697affdfa14212fe984b240f80cc5f328b1fad
zyg18 <93242287+zyg18@users.noreply.github.com>
2025-04-24 00:38:01 +0800

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
 data_files=(
-    large_netlist_10000000.txt
-    large_netlist_20000000.txt
-    large_netlist_30000000.txt
-    large_netlist_40000000.txt
-    large_netlist_50000000.txt
-    large_netlist_60000000.txt
+    large_netlist_1.txt
+    large_netlist_2.txt
+    large_netlist_3.txt
+    large_netlist_4.txt
 )
 
 for data in "${data_files[@]}"; do
     echo "Processing $data"
-    ./our_method_without_fixed_nodes "$data"
+    ./MFSPart "$data"
 done
 
 echo "All done!"
